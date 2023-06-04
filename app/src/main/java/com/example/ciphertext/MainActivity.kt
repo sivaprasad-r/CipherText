276ce0f4d0cb969b362ec59bf2572b726a10a354
Sivaprasad R <sivaprasadr91@gmail.com>
2023-06-04 12:06:25 +0530

    initial commit

---
+++ app/src/main/java/com/example/ciphertext/MainActivity.kt
@@ -0,0 +1,51 @@
+package com.example.ciphertext
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.example.ciphertext.HomeFragment
+import com.example.ciphertext.EncryptDecryptFragment
+import com.example.ciphertext.MeFragment
+import com.example.ciphertext.R
+
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var bottomNavigationView: BottomNavigationView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        bottomNavigationView = findViewById(R.id.bottom_navigation)
+        bottomNavigationView.setOnItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.navigation_home -> {
+                    replaceFragment(HomeFragment())
+                    true
+                }
+                R.id.navigation_encrypt_decrypt -> {
+                    replaceFragment(EncryptDecryptFragment())
+                    true
+                }
+                R.id.navigation_me -> {
+                    replaceFragment(MeFragment())
+                    true
+                }
+                else -> false
+            }
+        }
+
+
+
+        // Set the initial fragment
+        replaceFragment(HomeFragment())
+    }
+
+    private fun replaceFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragment_container, fragment)
+            .commit()
+    }
+}
