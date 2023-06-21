b01557b6b0be05074988b624fc90fee2a75c01b5
Sivaprasad R <sivaprasadr91@gmail.com>
2023-06-21 20:50:03 +0530

    added buttons and app icon

--- app/src/main/java/com/example/ciphertext/EncryptDecryptFragment.kt
+++ app/src/main/java/com/example/ciphertext/EncryptDecryptFragment.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 
+
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
 private const val ARG_PARAM1 = "param1"
@@ -36,7 +37,6 @@ class EncryptDecryptFragment : Fragment() {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_encrypt_decrypt, container, false)
     }
-
     companion object {
         /**
          * Use this factory method to create a new instance of
