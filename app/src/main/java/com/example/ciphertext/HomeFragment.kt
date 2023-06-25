da4f3a62e2c624e3d1778ca347237ebf535f006e
Sivaprasad R <sivaprasadr91@gmail.com>
2023-06-25 07:10:27 +0530

    Fixed TextView refresh problem

--- app/src/main/java/com/example/ciphertext/HomeFragment.kt
+++ app/src/main/java/com/example/ciphertext/HomeFragment.kt
@@ -17,74 +17,64 @@ import android.widget.TextView
 
 class HomeFragment : Fragment() {
 
+    private lateinit var tvScannedData: TextView
+    private lateinit var fileName: String
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
         val view = inflater.inflate(R.layout.fragment_home, container, false)
 
-        // Find the Floating Action Button
         val fab: FloatingActionButton = view.findViewById(R.id.fab)
 
-        // Set a click listener for the Floating Action Button
         fab.setOnClickListener {
-            // Initialize the IntentIntegrator
             val integrator = IntentIntegrator.forSupportFragment(this)
-
-            // Set desired properties for the QR Code scanner
             integrator.setPrompt("Scan a QR Code")
             integrator.setOrientationLocked(false)
             integrator.initiateScan()
         }
 
+        tvScannedData = view.findViewById(R.id.tvScannedData)
+        fileName = "scanned_data.txt"
+
         return view
     }
 
-    // Handle the result of the QR Code scanning
-    @Deprecated("Deprecated in Java")
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        displayScannedData()
+    }
+
+    private fun displayScannedData() {
+        val file = File(requireContext().filesDir, fileName)
+
+        if (file.exists()) {
+            val fileContents = file.readText(Charsets.UTF_8)
+            tvScannedData.text = fileContents
+        } else {
+            tvScannedData.visibility = View.GONE
+        }
+    }
+
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
 
         val result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data)
         if (result != null) {
             if (result.contents != null) {
-                // QR Code scan successful, handle the scanned data
                 val scannedData = result.contents
-                // Process the scanned data as needed
-                val fileName = "scanned_data.txt"
                 val fileContents = scannedData.toByteArray()
                 val file = File(requireContext().filesDir, fileName)
                 file.writeBytes(fileContents)
-                // ...
 
+                // Refresh the fragment by calling displayScannedData()
+                displayScannedData()
             } else {
-                // QR Code scan cancelled or failed
                 // Handle the cancellation or failure scenario
                 // ...
             }
         }
     }
-    //Read and display the content of file
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-
-        val tvScannedData: TextView = view.findViewById(R.id.tvScannedData)
-
-        val fileName = "scanned_data.txt"
-        val file = File(requireContext().filesDir, fileName)
-
-        if (file.exists()) {
-            // Read the contents of scanned_data.txt file
-            val fileContents = file.readText(Charsets.UTF_8)
-
-            // Set the contents to the TextView
-            tvScannedData.text = fileContents
-        } else {
-            // File does not exist, handle the scenario accordingly
-            // For example, you can display a message or hide the TextView
-            tvScannedData.visibility = View.GONE
-        }
-    }
-
 }
