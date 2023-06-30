0664d21e7c7acc46fdb8329962de4a366d9a8458
Sivaprasad R <sivaprasadr91@gmail.com>
2023-06-30 19:17:44 +0530

    removed dead code EncryptDecryptFragment

--- app/src/main/java/com/example/ciphertext/EncryptDecryptFragment.kt
+++ app/src/main/java/com/example/ciphertext/EncryptDecryptFragment.kt
@@ -11,24 +11,11 @@ import android.widget.EditText
 import java.io.File
 
 
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
 class EncryptDecryptFragment : Fragment() {
-    private var param1: String? = null
-    private var param2: String? = null
 
     private lateinit var etTextArea: EditText
     private lateinit var btnEncrypt: Button
     private lateinit var btnDecrypt: Button
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -71,16 +58,5 @@ class EncryptDecryptFragment : Fragment() {
         val privateKey = file.readText(Charsets.UTF_8).toByteArray()
         val decryptedText = CryptoUtils.decryptData(privateKey, cipherText)
         return decryptedText.toString()
-    }
-
-    companion object {
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            EncryptDecryptFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
     }
 }
